5dd43e7ae104365530aaedc7425628f23100fbb9
Alvaro Andrade <varito1@gmail.com>
2023-01-31 01:22:24 -0300

    :tada: Initial commit

---
+++ routes/greet/[name].tsx
@@ -0,0 +1,11 @@
+import { PageProps } from "$fresh/server.ts";
+
+export default function GreetPage(props: PageProps) {
+  const { name } = props.params;
+
+  return (
+    <main>
+      <p>Greeting to you, {name}!</p>
+    </main>
+  );
+}
